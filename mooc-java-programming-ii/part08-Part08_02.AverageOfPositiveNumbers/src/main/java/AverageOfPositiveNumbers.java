8bcb397596e44566879562d435b51c5de38f91f2
Mehmet Copur <mehmet.copur@powercoders.org>
2021-06-26 18:03:18 +0200

    recap1

--- mooc-java-programming-ii/part08-Part08_02.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
+++ mooc-java-programming-ii/part08-Part08_02.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
@@ -6,5 +6,35 @@ public class AverageOfPositiveNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+
+        int sum=0;
+        int count=0;
+
+        while (true){
+            int num=Integer.valueOf(scanner.nextLine());
+
+            if (num==0){
+                break;
+            }
+
+            if (num>0){
+                sum+=num;
+                count++;
+            }
+
+        }
+
+
+        if (sum<=0){
+
+            System.out.println("Cannot calculate the average");
+
+        }else {
+
+            System.out.println((sum*1.0)/count);
+
+        }
+
+
     }
 }
