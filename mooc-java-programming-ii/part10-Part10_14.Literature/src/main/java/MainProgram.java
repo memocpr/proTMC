bccb54f6bd6e5d371f3574ef26cb47356ecfaaf4
Mehmet Copur <mehmet.copur@powercoders.org>
2021-07-22 13:24:17 +0200

    regular expression

--- mooc-java-programming-ii/part10-Part10_14.Literature/src/main/java/MainProgram.java
+++ mooc-java-programming-ii/part10-Part10_14.Literature/src/main/java/MainProgram.java
@@ -1,10 +1,39 @@
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class MainProgram {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        ArrayList<Book> books=new ArrayList<>();
+
+        while (true){
+            System.out.println("Input the name of the book, empty stops:");
+            String name=scanner.nextLine();
+            if (name.isEmpty()){
+                break;
+            }
+
+            System.out.println("Input the age recommendation:");
+            int age=Integer.valueOf(scanner.nextLine());
+
+            books.add(new Book(name, age));
+        }
+
+        // compare the books according to recommendation age and name
+        Comparator<Book> comparator=Comparator
+                .comparing(Book::getAge)
+                .thenComparing(Book::getName);
+
+        Collections.sort(books, comparator);
+
+        System.out.println(books.size()+" books in total.");
+        System.out.println("Books:");
+        books.stream()
+                .forEach(book -> System.out.println(book));
 
     }
 
--- mooc-java-programming-ii/part10-Part10_15.RegularExpressions/src/main/java/MainProgram.java
+++ mooc-java-programming-ii/part10-Part10_15.RegularExpressions/src/main/java/MainProgram.java
@@ -1,9 +1,23 @@
-
+import java.util.Scanner;
 
 public class MainProgram {
 
     public static void main(String[] args) {
         // you can create test code here. Call the methods that you'll implement
         // during the course of this exercise
+
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("Enter a string:");
+
+        String input=sc.nextLine();
+
+        Checker checker=new Checker();
+
+        if (checker.timeOfDay(input)){
+            System.out.println("The form is correct.");
+        }else {
+            System.out.println("The form is incorrect.");
+        }
     }
 }
